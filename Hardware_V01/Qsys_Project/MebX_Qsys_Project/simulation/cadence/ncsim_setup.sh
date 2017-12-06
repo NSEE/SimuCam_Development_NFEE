5a53a790864e0f052317e05ba6821b9838fb4de1
Rodrigo França <rodmarfran@gmail.com>
2017-12-05 22:37:00 -0200

    Gabinete novo testado com sucesso

--- Hardware_V01/Qsys_Project/MebX_Qsys_Project/simulation/cadence/ncsim_setup.sh
+++ Hardware_V01/Qsys_Project/MebX_Qsys_Project/simulation/cadence/ncsim_setup.sh
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 16.1 196 win32 2017.10.02.19:43:04
+# ACDS 16.1 196 win32 2017.12.05.15:49:59
 
 # ----------------------------------------
 # ncsim - auto-generated simulation script
@@ -101,7 +101,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 16.1 196 win32 2017.10.02.19:43:04
+# ACDS 16.1 196 win32 2017.12.05.15:49:59
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="MebX_Qsys_Project"
