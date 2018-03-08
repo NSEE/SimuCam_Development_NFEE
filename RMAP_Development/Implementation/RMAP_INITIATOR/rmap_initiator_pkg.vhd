c7c914c5c2daa709114fc1c34ca96f8393d89c93
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-03-08 15:54:52 -0300

    Limpeza de arquivos

--- RMAP_Development/Implementation/RMAP_INITIATOR/rmap_initiator_pkg.vhd
+++
@@ -1,7 +0,0 @@
-package rmap_initiator_pkg is
-	
-end package rmap_initiator_pkg;
-
-package body rmap_initiator_pkg is
-	
-end package body rmap_initiator_pkg;
--- RMAP_Development/Implementation/RMAP_MEMORY/rmap_memory_pkg.vhd
+++
@@ -1,7 +0,0 @@
-package rmap_memory_pkg is
-	
-end package rmap_memory_pkg;
-
-package body rmap_memory_pkg is
-	
-end package body rmap_memory_pkg;
