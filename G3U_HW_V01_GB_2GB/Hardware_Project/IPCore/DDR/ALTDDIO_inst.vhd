955ceff4d7120e9b6cdeb5ab99d80f5259d5ee55
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-04-20 14:28:49 -0300

    Reorganização de pastas e remoção de projetos de outros gabinetes (movidos para outros branchs)

--- G3U_HW_V01_GB_2GB/Hardware_Project/IPCore/DDR/ALTDDIO_inst.vhd
+++
@@ -1,6 +0,0 @@
-ALTDDIO_inst : ALTDDIO PORT MAP (
-		datain	 => datain_sig,
-		inclock	 => inclock_sig,
-		dataout_h	 => dataout_h_sig,
-		dataout_l	 => dataout_l_sig
-	);
--- G3U_HW_V01_GB_2GB/README.md
+++
@@ -1,2 +0,0 @@
-# Hardware V01
-Initial Hardware for DDR2, DMA, Leds, Temp and Power tests
