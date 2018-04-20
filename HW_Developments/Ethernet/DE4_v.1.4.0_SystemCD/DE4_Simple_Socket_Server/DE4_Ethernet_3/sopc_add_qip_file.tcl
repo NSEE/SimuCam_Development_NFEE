230bd3345e9f472528d65c90164777aa3b657d28
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-04-20 17:27:30 -0300

    Adicionados arquivos de referencia do Ethernet

---
+++ HW_Developments/Ethernet/DE4_v.1.4.0_SystemCD/DE4_Simple_Socket_Server/DE4_Ethernet_3/sopc_add_qip_file.tcl
@@ -0,0 +1,3 @@
+project_open -current_revision "D:/SVN/de4/test/cd_demo_version/Q11.1_sp1_SOPC/DE4_230/DE4_Simple_Socket_Server/DE4_Ethernet_0/DE4_Ethernet.qpf"
+set_global_assignment -name QIP_FILE "DE4_SOPC.qip"
+project_close
