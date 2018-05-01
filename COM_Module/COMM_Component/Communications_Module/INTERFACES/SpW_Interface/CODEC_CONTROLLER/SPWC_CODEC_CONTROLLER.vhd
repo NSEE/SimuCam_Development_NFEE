4b03fa25def7ade757bf317f1fe9feaff6ab1570
Rodrigo França <rodmarfran@gmail.com>
2018-05-01 01:00:24 -0300

    Preparado moduo com backdoor correto, precisa simular

--- COM_Module/COMM_Component/Communications_Module/INTERFACES/SpW_Interface/CODEC_CONTROLLER/SPWC_CODEC_CONTROLLER.vhd
+++ COM_Module/COMM_Component/Communications_Module/INTERFACES/SpW_Interface/CODEC_CONTROLLER/SPWC_CODEC_CONTROLLER.vhd
@@ -155,31 +155,30 @@ architecture spwc_codec_controller_arc of spwc_codec_controller_ent is
 	signal timecode_tick_in_trigger_sig  : std_logic := '0';
 	signal codec_timecode_tx_tick_in_sig : std_logic := '0';
 
-	-- Signals for Backdoor Mode
-	signal spwc_mm_backdoor_read_registers : spwc_mm_read_registers_type;
-
 begin
 
 	-- SpaceWire Light Codec Encapsulation Component (Loopback Version)
 	spwc_codec_loopback_ent_inst : entity work.spwc_codec_loopback_ent
 		port map(
-			clk_100                       => clk100,
-			clk_200                       => clk200,
-			rst                           => rst,
-			spwc_codec_reset              => spwc_codec_reset_in_sig,
-			spwc_mm_write_registers       => spwc_mm_write_registers,
-			spwc_mm_read_registers        => spwc_mm_backdoor_read_registers,
-			spwc_codec_link_command_in    => spwc_codec_link_command_in_sig,
-			spwc_codec_link_status_out    => spwc_codec_link_status_out_sig,
-			spwc_codec_ds_encoding_rx_in  => spwc_codec_ds_encoding_rx_in_sig,
-			spwc_codec_ds_encoding_tx_out => spwc_codec_ds_encoding_tx_out_sig,
-			spwc_codec_link_error_out     => spwc_codec_link_error_out_sig,
-			spwc_codec_timecode_rx_out    => spwc_codec_timecode_rx_out_sig,
-			spwc_codec_timecode_tx_in     => spwc_codec_timecode_tx_in_sig,
-			spwc_codec_data_rx_in         => spwc_codec_data_rx_in_sig,
-			spwc_codec_data_rx_out        => spwc_codec_data_rx_out_sig,
-			spwc_codec_data_tx_in         => spwc_codec_data_tx_in_sig,
-			spwc_codec_data_tx_out        => spwc_codec_data_tx_out_sig
+			clk_100                                            => clk100,
+			clk_200                                            => clk200,
+			rst                                                => rst,
+			spwc_codec_reset                                   => spwc_codec_reset_in_sig,
+			spwc_mm_write_registers                            => spwc_mm_write_registers,
+			spwc_mm_read_registers.RX_BACKDOOR_STATUS_REGISTER => spwc_mm_read_registers.RX_BACKDOOR_STATUS_REGISTER,
+			spwc_mm_read_registers.RX_BACKDOOR_DATA_REGISTER   => spwc_mm_read_registers.RX_BACKDOOR_DATA_REGISTER,
+			spwc_mm_read_registers.TX_BACKDOOR_STATUS_REGISTER => spwc_mm_read_registers.TX_BACKDOOR_STATUS_REGISTER,
+			spwc_codec_link_command_in                         => spwc_codec_link_command_in_sig,
+			spwc_codec_link_status_out                         => spwc_codec_link_status_out_sig,
+			spwc_codec_ds_encoding_rx_in                       => spwc_codec_ds_encoding_rx_in_sig,
+			spwc_codec_ds_encoding_tx_out                      => spwc_codec_ds_encoding_tx_out_sig,
+			spwc_codec_link_error_out                          => spwc_codec_link_error_out_sig,
+			spwc_codec_timecode_rx_out                         => spwc_codec_timecode_rx_out_sig,
+			spwc_codec_timecode_tx_in                          => spwc_codec_timecode_tx_in_sig,
+			spwc_codec_data_rx_in                              => spwc_codec_data_rx_in_sig,
+			spwc_codec_data_rx_out                             => spwc_codec_data_rx_out_sig,
+			spwc_codec_data_tx_in                              => spwc_codec_data_tx_in_sig,
+			spwc_codec_data_tx_out                             => spwc_codec_data_tx_out_sig
 		);
 
 	-- CLK100 Commands DC FIFO Component
@@ -557,11 +556,5 @@ begin
 	interrupts_drivers_current_sig.TIMECODE_RECEIVED <= timecode_tick_out_sig;
 	-- Timecode Received Rising Edge Signal assingment
 	interrupts_flags_sig.TIMECODE_RECEIVED           <= ('1') when ((interrupts_drivers_current_sig.TIMECODE_RECEIVED = '1') and (interrupts_drivers_past_sig.TIMECODE_RECEIVED = '0')) else ('0');
-
-	-- Backdoor signals
-	spwc_mm_read_registers.RX_BACKDOOR_STATUS_REGISTER.CODEC_DATAVALID_READY <= spwc_mm_backdoor_read_registers.RX_BACKDOOR_STATUS_REGISTER.CODEC_DATAVALID_READY;
-	spwc_mm_read_registers.RX_BACKDOOR_DATA_REGISTER.CODEC_SPW_FLAG          <= spwc_mm_backdoor_read_registers.RX_BACKDOOR_DATA_REGISTER.CODEC_SPW_FLAG;
-	spwc_mm_read_registers.RX_BACKDOOR_DATA_REGISTER.CODEC_SPW_DATA          <= spwc_mm_backdoor_read_registers.RX_BACKDOOR_DATA_REGISTER.CODEC_SPW_DATA;
-	spwc_mm_read_registers.TX_BACKDOOR_STATUS_REGISTER.CODEC_DATAVALID_READY <= spwc_mm_backdoor_read_registers.TX_BACKDOOR_STATUS_REGISTER.CODEC_DATAVALID_READY;
 
 end architecture spwc_codec_controller_arc;
