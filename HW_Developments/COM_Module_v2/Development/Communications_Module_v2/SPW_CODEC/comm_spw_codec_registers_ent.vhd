3b532b44b27c40584871422b5bad6e78ffb34f86
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-05-15 11:37:35 -0300

    Criação da estrutura básica do SPW_CODEC

---
+++ HW_Developments/COM_Module_v2/Development/Communications_Module_v2/SPW_CODEC/comm_spw_codec_registers_ent.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity comm_spw_codec_registers_ent is
+	port(
+		clk_i : in std_logic;
+		rst_i : in std_logic
+	);
+end entity comm_spw_codec_registers_ent;
+
+architecture RTL of comm_spw_codec_registers_ent is
+	
+begin
+
+end architecture RTL;
---
+++ HW_Developments/COM_Module_v2/Development/Communications_Module_v2/comm_spw_codec_pkg.vhd
@@ -0,0 +1,7 @@
+package comm_spw_codec_pkg is
+	
+end package comm_spw_codec_pkg;
+
+package body comm_spw_codec_pkg is
+	
+end package body comm_spw_codec_pkg;
