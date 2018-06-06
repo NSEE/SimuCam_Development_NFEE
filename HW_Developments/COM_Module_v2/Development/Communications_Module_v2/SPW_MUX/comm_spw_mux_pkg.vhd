cdd358ed86c5135171141c70933da6c0a6f8251e
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-06-06 12:14:08 -0300

    Modificações no código

--- HW_Developments/COM_Module_v2/Development/Communications_Module_v2/SPW_MUX/comm_spw_mux_pkg.vhd
+++ HW_Developments/COM_Module_v2/Development/Communications_Module_v2/SPW_MUX/comm_spw_mux_pkg.vhd
@@ -1,8 +1,16 @@
+use work.comm_spw_codec_pkg.all;
+
 package comm_spw_mux_pkg is
 
 	----------------------------------------------------
 	-- SpW Mux Block
 	----------------------------------------------------
+
+	-- Rx Demux Channel Quantity
+	constant c_RX_DEMUX_CHANNELS : natural := 2;
+
+	-- Tx Mux Channel Quantity
+	constant c_TX_MUX_CHANNELS : natural := 3;
 
 	----------------------------------------------------
 	-- Configuration Registers
@@ -20,20 +28,23 @@ package comm_spw_mux_pkg is
 
 	-- Rx Demux Channels Status Signals
 	type t_swdc_status is record
-		idle : std_logic;
+		overflow : std_logic;
 	end record t_swdc_status;
 
 	-- Rx Demux Control Signals
-	type t_swrd_control is record
-		channel_0 : t_swdc_control;
-		channel_1 : t_swdc_control;
-	end record t_swrd_control;
+	type t_swrd_control is array (0 to (c_RX_DEMUX_CHANNELS - 1)) of t_swdc_control;
 
 	-- Rx Demux Status Signals
-	type t_swrd_status is record
-		channel_0 : t_swdc_status;
-		channel_1 : t_swdc_status;
-	end record t_swrd_status;
+	type t_swrd_status is array (0 to (c_RX_DEMUX_CHANNELS - 1)) of t_swdc_status;
+
+	-- Rx Demux SpW Control Signals
+	type t_swrd_spw_control is array (0 to (c_RX_DEMUX_CHANNELS - 1)) of t_spwl_data_rx_control;
+
+	-- Rx Demux SpW Status Signals
+	type t_swrd_spw_status is array (0 to (c_RX_DEMUX_CHANNELS - 1)) of t_spwl_data_rx_status;
+
+	-- Rx Demux SpW Payload Signals
+	type t_swrd_spw_payload is array (0 to (c_RX_DEMUX_CHANNELS - 1)) of t_spwl_data_payload;
 
 	----------------------------------------------------
 	-- Tx Mux
@@ -42,59 +53,27 @@ package comm_spw_mux_pkg is
 	-- Tx Mux Channels Control Signals
 	type t_swmc_control is record
 		enable : std_logic;
-		master : std_logic;
 	end record t_swmc_control;
 
 	-- Tx Mux Channels Status Signals
 	type t_swmc_status is record
-		idle : std_logic;
+		queued : std_logic;
 	end record t_swmc_status;
 
 	-- Tx Mux Control Signals
-	type t_swtm_control is record
-		channel_0 : t_swmc_control;
-		channel_1 : t_swmc_control;
-		channel_2 : t_swmc_control;
-	end record t_swtm_control;
+	type t_swtm_control is array (0 to (c_TX_MUX_CHANNELS - 1)) of t_swmc_control;
 
 	-- Tx Mux Status Signals
-	type t_swtm_status is record
-		channel_0 : t_swmc_status;
-		channel_1 : t_swmc_status;
-		channel_2 : t_swmc_status;
-	end record t_swtm_status;
+	type t_swtm_status is array (0 to (c_TX_MUX_CHANNELS - 1)) of t_swmc_status;
 
-	----------------------------------------------------
-	-- SpW Codec
-	----------------------------------------------------
+	-- Tx Mux SpW Control Signals
+	type t_swtm_spw_control is array (0 to (c_TX_MUX_CHANNELS - 1)) of t_spwl_data_tx_control;
 
-	-- Codec Receiver Data Control Signals
-	type t_spwl_data_rx_control is record
-		rxread : std_logic;
-	end record t_spwl_data_rx_control;
+	-- Tx Mux SpW Status Signals
+	type t_swtm_spw_status is array (0 to (c_TX_MUX_CHANNELS - 1)) of t_spwl_data_tx_status;
 
-	-- Codec Receiver Data Status Signals
-	type t_spwl_data_rx_status is record
-		rxvalid : std_logic;
-		rxhalff : std_logic;
-	end record t_spwl_data_rx_status;
-
-	-- Codec Transmitter Data Control Signals
-	type t_spwl_data_tx_control is record
-		txwrite : std_logic;
-	end record t_spwl_data_tx_control;
-
-	-- Codec Transmitter Data Status Signals
-	type t_spwl_data_tx_status is record
-		txrdy   : std_logic;
-		txhalff : std_logic;
-	end record t_spwl_data_tx_status;
-
-	-- Codec Data Payload Signals
-	type t_spwl_data_payload is record
-		flag : std_logic;
-		data : std_logic_vector(7 downto 0);
-	end record t_spwl_data_payload;
+	-- Tx Mux SpW Payload Signals
+	type t_swtm_spw_payload is array (0 to (c_TX_MUX_CHANNELS - 1)) of t_spwl_data_payload;
 
 end package comm_spw_mux_pkg;
 
--- HW_Developments/COM_Module_v2/Development/Communications_Module_v2/SPW_MUX/registers/comm_spw_mux_registers_ent.vhd
+++ HW_Developments/COM_Module_v2/Development/Communications_Module_v2/SPW_MUX/registers/comm_spw_mux_registers_ent.vhd
@@ -2,13 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.comm_spw_mux_pkg.all;
+
 entity comm_spw_mux_registers_ent is
 	port(
 		-- basic inputs
-		clk_i : in std_logic;
-		rst_i : in std_logic
+		clk_i          : in  std_logic;
+		rst_i          : in  std_logic;
 		-- logic inputs
+		swrd_status_i  : in  t_swrd_status;
+		swtm_status_i  : in  t_swtm_status;
 		-- logic outputs
+		swrd_control_o : out t_swrd_control;
+		swtm_control_o : out t_swtm_control
 	);
 end entity comm_spw_mux_registers_ent;
 
