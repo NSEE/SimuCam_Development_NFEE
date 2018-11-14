d1b5eb76e0b2a86a607dfe673769fe587f04bb1d
Rodrigo de Marca França <rodmarfran@gmail.com>
2018-11-14 18:19:51 -0200

    Adicionados arquivos base do projeto

---
+++ FPGA_Developments/COM_Module_v1.1/Development/Communications_Module_v1.1/comm_v1_1_top.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity comm_v1_1_top is
+	port(
+		clk_i : in std_logic;
+		rst_i : in std_logic
+	);
+end entity comm_v1_1_top;
+
+architecture RTL of comm_v1_1_top is
+	
+	-- constants
+	-- signals
+	
+begin
+	
+	-- spw codec instantiation
+	
+	-- spw mux/demux instantiation
+	
+	-- rmap codec instantiation
+	
+	-- rmap memory area instantiation
+	
+		
+end architecture RTL;
