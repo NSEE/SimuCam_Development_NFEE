889f53c26b89550705c7aa020d34735f236a3ba3
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-01-26 14:40:08 -0200

    terminado masking. Falta adicionar os delays.será realizado posteriormente

--- FPGA_Developments/COM_Module_v1_8/Development/Communications_Module_v1_8/FEE_DATA_CONTROLLER/MASKING_MACHINE/masking_machine_ent.vhd
+++ FPGA_Developments/COM_Module_v1_8/Development/Communications_Module_v1_8/FEE_DATA_CONTROLLER/MASKING_MACHINE/masking_machine_ent.vhd
@@ -7,6 +7,10 @@ entity masking_machine_ent is
 	port(
 		clk_i                         : in  std_logic;
 		rst_i                         : in  std_logic;
+--		ccd_column_qtd_i              : in  std_logic;
+--		line_delay_i                  : in  std_logic_vector(15 downto 0);
+--		column_delay_i                : in  std_logic_vector(15 downto 0);
+--		adc_delay_i                   : in  std_logic_vector(15 downto 0);
 		current_timecode_i            : in  std_logic_vector(7 downto 0);
 		window_data_i                 : in  std_logic_vector(63 downto 0);
 		window_mask_i                 : in  std_logic_vector(63 downto 0);
@@ -24,6 +28,7 @@ end entity masking_machine_ent;
 
 architecture RTL of masking_machine_ent is
 
+	-- function to change the timecode from the pattern pixels
 	function f_pixel_msb_change_timecode(pixel_msb_i : in std_logic_vector; timecode_i : in std_logic_vector) return std_logic_vector is
 		variable v_new_pixel_msb : std_logic_vector(7 downto 0);
 	begin
@@ -51,7 +56,21 @@ architecture RTL of masking_machine_ent is
 		return v_new_pixel_msb;
 	end function f_pixel_msb_change_timecode;
 
+	-- masking fifo record type
+	type t_masking_fifo is record
+		data  : std_logic_vector(7 downto 0);
+		sclr  : std_logic;
+		wrreq : std_logic;
+		full  : std_logic;
+		usedw : std_logic_vector(14 downto 0);
+	end record t_masking_fifo;
+
+	-- masking fifo signals
+	signal s_masking_fifo : t_masking_fifo;
+
 	-- TODO: adicionar delays
+
+	-- masking machine fsm type
 	type t_masking_machine_fsm is (
 		IDLE,
 		MASK_FETCH,
@@ -69,6 +88,7 @@ architecture RTL of masking_machine_ent is
 		--		ADC_DELAY,
 	);
 
+	-- masking machine fsm state
 	signal s_masking_machine_state : t_masking_machine_fsm;
 
 	signal s_registered_window_data : std_logic_vector(63 downto 0);
@@ -80,14 +100,29 @@ architecture RTL of masking_machine_ent is
 
 begin
 
+	-- masking buffer instantiation
+	masking_machine_sc_fifo_inst : entity work.masking_machine_sc_fifo
+		port map(
+			aclr  => rst_i,
+			clock => clk_i,
+			data  => s_masking_fifo.data,
+			rdreq => masking_buffer_rdreq_i,
+			sclr  => s_masking_fifo.sclr,
+			wrreq => s_masking_fifo.wrreq,
+			empty => masking_buffer_empty_o,
+			full  => s_masking_fifo.full,
+			q     => masking_buffer_rddata_o,
+			usedw => s_masking_fifo.usedw
+		);
+
 	p_masking_machine : process(clk_i, rst_i) is
 	begin
 		if (rst_i = '1') then
 			window_data_read_o       <= '0';
 			window_mask_read_o       <= '0';
-			spw_txwrite_o            <= '0';
-			spw_txflag_o             <= '0';
-			spw_txdata_o             <= (others => '0');
+			s_masking_fifo.data      <= (others => '0');
+			s_masking_fifo.rdreq     <= '0';
+			s_masking_fifo.wrreq     <= '0';
 			s_masking_machine_state  <= IDLE;
 			s_registered_window_data <= (others => '0');
 			s_registered_window_mask <= (others => '0');
@@ -95,12 +130,10 @@ begin
 			s_delay                  <= '0';
 		elsif rising_edge(clk_i) then
 
-			window_data_read_o <= '0';
-			window_mask_read_o <= '0';
-			spw_txwrite_o      <= '0';
-			spw_txflag_o       <= '0';
-			spw_txdata_o       <= (others => '0');
-			s_delay            <= '0';
+			window_data_read_o      <= '0';
+			window_mask_read_o      <= '0';
+			s_masking_machine_state <= IDLE;
+			s_delay                 <= '0';
 
 			-- TODO: adicionar delays
 			case (s_masking_machine_state) is
@@ -146,26 +179,26 @@ begin
 
 				when PIXEL_0_BYTE_MSB =>
 					s_masking_machine_state <= PIXEL_0_BYTE_MSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= f_pixel_msb_change_timecode(s_registered_window_data(7 downto 0), current_timecode_i);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= f_pixel_msb_change_timecode(s_registered_window_data(7 downto 0), current_timecode_i);
 						end if;
 						s_masking_machine_state <= PIXEL_0_BYTE_LSB;
 					end if;
 
 				when PIXEL_0_BYTE_LSB =>
 					s_masking_machine_state <= PIXEL_0_BYTE_LSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= s_registered_window_data(15 downto 8);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= s_registered_window_data(15 downto 8);
 						end if;
 						s_mask_counter          <= s_mask_counter + 1;
 						s_masking_machine_state <= PIXEL_1_BYTE_MSB;
@@ -173,26 +206,26 @@ begin
 
 				when PIXEL_1_BYTE_MSB =>
 					s_masking_machine_state <= PIXEL_1_BYTE_MSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= f_pixel_msb_change_timecode(s_registered_window_data(23 downto 16), current_timecode_i);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= f_pixel_msb_change_timecode(s_registered_window_data(23 downto 16), current_timecode_i);
 						end if;
 						s_masking_machine_state <= PIXEL_1_BYTE_LSB;
 					end if;
 
 				when PIXEL_1_BYTE_LSB =>
 					s_masking_machine_state <= PIXEL_1_BYTE_LSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= s_registered_window_data(31 downto 24);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= s_registered_window_data(31 downto 24);
 						end if;
 						s_mask_counter          <= s_mask_counter + 1;
 						s_masking_machine_state <= PIXEL_2_BYTE_MSB;
@@ -200,26 +233,26 @@ begin
 
 				when PIXEL_2_BYTE_MSB =>
 					s_masking_machine_state <= PIXEL_2_BYTE_MSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= f_pixel_msb_change_timecode(s_registered_window_data(39 downto 32), current_timecode_i);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= f_pixel_msb_change_timecode(s_registered_window_data(39 downto 32), current_timecode_i);
 						end if;
 						s_masking_machine_state <= PIXEL_2_BYTE_LSB;
 					end if;
 
 				when PIXEL_2_BYTE_LSB =>
 					s_masking_machine_state <= PIXEL_2_BYTE_LSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= s_registered_window_data(47 downto 40);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= s_registered_window_data(47 downto 40);
 						end if;
 						s_mask_counter          <= s_mask_counter + 1;
 						s_masking_machine_state <= PIXEL_3_BYTE_MSB;
@@ -227,26 +260,26 @@ begin
 
 				when PIXEL_3_BYTE_MSB =>
 					s_masking_machine_state <= PIXEL_3_BYTE_MSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= f_pixel_msb_change_timecode(s_registered_window_data(55 downto 48), current_timecode_i);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= f_pixel_msb_change_timecode(s_registered_window_data(55 downto 48), current_timecode_i);
 						end if;
 						s_masking_machine_state <= PIXEL_3_BYTE_LSB;
 					end if;
 
 				when PIXEL_3_BYTE_LSB =>
 					s_masking_machine_state <= PIXEL_3_BYTE_LSB;
-					-- TODO: mudar para fifo
-					if ((spw_txrdy_i = '1') and (spw_txhalff_i = '0')) then
+					-- check if masking fifo is not full
+					if (s_masking_fifo.full = '0') then
+						-- masking fifo has space
 						-- check if the bit is masked
 						if (s_registered_window_mask(s_mask_counter) = '1') then
-							spw_txwrite_o <= '1';
-							spw_txflag_o  <= '0';
-							spw_txdata_o  <= s_registered_window_data(63 downto 56);
+							s_masking_fifo.wrreq <= '1';
+							s_masking_fifo.data  <= s_registered_window_data(63 downto 56);
 						end if;
 						-- check mask counter
 						if (s_mask_counter < 63) then
@@ -285,5 +318,13 @@ begin
 
 		end if;
 	end process p_masking_machine;
+
+	-- masking fifo sclear signal reset
+	s_masking_fifo.sclr <= ('1') when (rst_i = '1') else (masking_buffer_clear_i);
+
+	-- masking fifo almost empty signal
+	masking_buffer_almost_empty_o <= ('0') when (rst_i = '1')
+		else ('1') when (s_masking_fifo.usedw = std_logic_vector(to_unsigned(1, s_masking_fifo.usedw'length)))
+		else ('0');
 
 end architecture RTL;
