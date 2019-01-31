56825634d16f317618c13dec6fc06d48e4cf1e91
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-01-31 01:20:47 -0200

    Sintetizado HW com mascara de bit corrigida

--- G3U_HW_V02_2GB/UpdateHwProject.bat
+++ G3U_HW_V02_2GB/UpdateHwProject.bat
@@ -1,6 +1,6 @@
-@ECHO off
+﻿@ECHO off
 PUSHD "%~dp0"
 REM Executa os batch files responsaveis pelo update de cada módulo o Hw Project
-START cmd /k "..\FPGA_Developments\COM_Module_v1_8\Development\UpdateComm180Hw.bat"
+START cmd /c "..\FPGA_Developments\COM_Module_v1_8\Development\UpdateComm180Hw.bat"
 REM Adicionar novos Hw sempre que forem criados
 REM PAUSE
