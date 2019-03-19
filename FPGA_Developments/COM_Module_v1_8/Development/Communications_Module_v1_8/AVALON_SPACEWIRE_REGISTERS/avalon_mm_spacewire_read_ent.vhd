08103f0ce4a7355eb6d5a5c0c6aa02319dcf1c31
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-03-19 16:22:27 -0300

    Generated HW V26 with a correction in the empty flags read

--- FPGA_Developments/COM_Module_v1_8/Development/Communications_Module_v1_8/AVALON_SPACEWIRE_REGISTERS/avalon_mm_spacewire_read_ent.vhd
+++ FPGA_Developments/COM_Module_v1_8/Development/Communications_Module_v1_8/AVALON_SPACEWIRE_REGISTERS/avalon_mm_spacewire_read_ent.vhd
@@ -145,7 +145,7 @@ begin
 					avalon_mm_spacewire_o.readdata(9)           <= spacewire_read_registers_i.comm_irq_flags_reg.comm_right_buffer_1_empty_flag;
 					avalon_mm_spacewire_o.readdata(10)          <= spacewire_read_registers_i.comm_irq_flags_reg.comm_left_buffer_0_empty_flag;
 					avalon_mm_spacewire_o.readdata(11)          <= spacewire_read_registers_i.comm_irq_flags_reg.comm_left_buffer_1_empty_flag;
-					avalon_mm_spacewire_o.readdata(31 downto 9) <= (others => '0');
+					avalon_mm_spacewire_o.readdata(31 downto 12) <= (others => '0');
 				when (16#13#) =>
 					avalon_mm_spacewire_o.readdata(0)            <= spacewire_write_registers_i.comm_irq_flags_clear_reg.comm_rmap_write_command_flag_clear;
 					avalon_mm_spacewire_o.readdata(7 downto 1)   <= (others => '0');
