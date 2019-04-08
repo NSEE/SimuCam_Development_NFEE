33b8402fc3c61923facb1e0032ba0940c1c3480e
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-04-08 18:54:33 -0300

    Generated HW V57 for the R0 release

--- G3U_HW_V02_2GB/Qsys_Project/software/Simucam_R0_UART_bsp/system.h
+++ G3U_HW_V02_2GB/Qsys_Project/software/Simucam_R0_UART_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'MebX_Qsys_Project'
  * SOPC Builder design path: ../../MebX_Qsys_Project.sopcinfo
  *
- * Generated: Mon Apr 08 14:07:48 BRT 2019
+ * Generated: Mon Apr 08 16:02:57 BRT 2019
  */
 
 /*
@@ -1185,7 +1185,7 @@
 #define SYSID_QSYS_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_NAME "/dev/sysid_qsys"
 #define SYSID_QSYS_SPAN 8
-#define SYSID_QSYS_TIMESTAMP 1554703993
+#define SYSID_QSYS_TIMESTAMP 1554743576
 #define SYSID_QSYS_TYPE "altera_avalon_sysid_qsys"
 
 
