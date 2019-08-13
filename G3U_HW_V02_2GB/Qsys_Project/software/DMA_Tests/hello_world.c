57f6ba00c2eed6ffba8f5d0da37122179fc940fa
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-08-13 13:40:06 -0300

    Unaligment access tests completed

--- G3U_HW_V02_2GB/Qsys_Project/software/DMA_Tests/hello_world.c
+++ G3U_HW_V02_2GB/Qsys_Project/software/DMA_Tests/hello_world.c
@@ -22,6 +22,19 @@
 
 FILE* fp;
 
+typedef struct TypesTest {
+
+
+	alt_8 bit8_0;
+	bool bool_1;
+	alt_8 bit8_1;
+	alt_32 bit32;
+	alt_16 bit16;
+	bool bool_0;
+
+
+} TTypesTest;
+
 int main() {
 	printf("Hello from Nios II!\n\n");
 
@@ -38,10 +51,36 @@ int main() {
 
 	usleep(1000 * 1000 * 1);
 
-	pxDes = (alt_u8 *) AVSTAP32_0_BASE;
-	*pxDes = (alt_u32) 50;
+	TTypesTest *xTypesTest = (TTypesTest *) AVSTAP32_0_BASE;
 
-	*(pxDes+3) = (alt_u32) 50;
+//	xTypesTest->bit8_0 = 0xFF;
+//	xTypesTest->bit16 = 0x5555;
+//	xTypesTest->bit8_1 = 0x33;
+//	xTypesTest->bit32 = 0x12345678;
+//	xTypesTest->bool_0 = false;
+//	xTypesTest->bool_1 = true;
+
+	pxDes32 = (alt_u32 *) AVSTAP32_0_BASE;
+	*(pxDes32 + 0) = (alt_u32) 0x000000FF;
+	*(pxDes32 + 1) = (alt_u32) 0x00000001;
+	*(pxDes32 + 2) = (alt_u32) 0x00000033;
+	*(pxDes32 + 3) = (alt_u32) 0x12345678;
+	*(pxDes32 + 4) = (alt_u32) 0x00005555;
+	*(pxDes32 + 5) = (alt_u32) 0x00000000;
+
+	printf("bit8_0: %02X \n", xTypesTest->bit8_0);
+	 printf("bit16: %04X \n", xTypesTest->bit16);
+	printf("bit8_1: %02X \n", xTypesTest->bit8_1);
+     printf("bit32: %08lX \n", xTypesTest->bit32);
+	printf("bool_0: %08X \n", xTypesTest->bool_0);
+	printf("bool_1: %08X \n", xTypesTest->bool_1);
+
+
+//	pxDes = (alt_u8 *) AVSTAP32_0_BASE;
+//	for (uiDataCnt = 0; uiDataCnt < 1024; uiDataCnt++) {
+//		*pxDes = (alt_u8) uiDataCnt;
+//		pxDes++;
+//	}
 
 //	pxDes32 = (alt_u32 *) AVSTAP32_0_BASE;
 //	for (uiDataCnt = 0; uiDataCnt < 1024; uiDataCnt++) {
@@ -52,7 +91,7 @@ int main() {
 	usleep(1000 * 1000 * 1);
 
 	pxDes32 = (alt_u32 *) AVSTAP32_0_BASE;
-	for (uiDataCnt = 0; uiDataCnt < 1024; uiDataCnt++) {
+	for (uiDataCnt = 0; uiDataCnt < 10; uiDataCnt++) {
 		printf("Addr: %04lu; Data: %08lX \n", (alt_u32) pxDes32, *pxDes32);
 		pxDes32++;
 	}
