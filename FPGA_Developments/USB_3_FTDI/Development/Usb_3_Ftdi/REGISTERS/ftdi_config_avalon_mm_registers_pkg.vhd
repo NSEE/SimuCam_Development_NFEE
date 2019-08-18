70d3400641dd7efd2f64214604bcd2a8ecdf71d9
Rodrigo França <rodmarfran@gmail.com>
2019-08-18 15:05:02 -0300

    Modifications for AvsData tests

--- FPGA_Developments/USB_3_FTDI/Development/Usb_3_Ftdi/REGISTERS/ftdi_config_avalon_mm_registers_pkg.vhd
+++ FPGA_Developments/USB_3_FTDI/Development/Usb_3_Ftdi/REGISTERS/ftdi_config_avalon_mm_registers_pkg.vhd
@@ -19,13 +19,33 @@ package ftdi_config_avalon_mm_registers_pkg is
 		rdready : std_logic;
 	end record t_ftdi_dbuffer_status_rd_reg;
 
+	type t_ftdi_test_fifo_control_wr_reg is record
+		tx_rdreq       : std_logic;
+		rx_wrdata_data : std_logic_vector(31 downto 0);
+		rx_wrdata_be   : std_logic_vector(3 downto 0);
+		rx_wrreq       : std_logic;
+	end record t_ftdi_test_fifo_control_wr_reg;
+
+	type t_ftdi_test_fifo_status_rd_reg is record
+		tx_rddata_data : std_logic_vector(31 downto 0);
+		tx_rddata_be   : std_logic_vector(3 downto 0);
+		tx_rdempty     : std_logic;
+		tx_rdfull      : std_logic;
+		tx_rdusedw     : std_logic_vector(11 downto 0);
+		rx_wrempty     : std_logic;
+		rx_wrfull      : std_logic;
+		rx_wrusedw     : std_logic_vector(11 downto 0);
+	end record t_ftdi_test_fifo_status_rd_reg;
+
 	type t_ftdi_config_write_registers is record
-		general_control_reg : t_ftdi_general_control_wr_reg;
+		general_control_reg   : t_ftdi_general_control_wr_reg;
+		test_fifo_control_reg : t_ftdi_test_fifo_control_wr_reg;
 	end record t_ftdi_config_write_registers;
 
 	type t_ftdi_config_read_registers is record
 		tx_dbuffer_status_reg : t_ftdi_dbuffer_status_rd_reg;
 		rx_dbuffer_status_reg : t_ftdi_dbuffer_status_rd_reg;
+		test_fifo_status_reg  : t_ftdi_test_fifo_status_rd_reg;
 	end record t_ftdi_config_read_registers;
 
 end package ftdi_config_avalon_mm_registers_pkg;
