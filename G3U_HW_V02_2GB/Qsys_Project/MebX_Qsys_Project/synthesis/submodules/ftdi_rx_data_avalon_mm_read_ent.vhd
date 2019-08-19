e3c8cc4bed600a0181bdef0375944fd342b06e2c
Rodrigo de Marca França <rodmarfran@gmail.com>
2019-08-18 21:40:35 -0300

    Synthetized a HW with the corrections

--- G3U_HW_V02_2GB/Qsys_Project/MebX_Qsys_Project/synthesis/submodules/ftdi_rx_data_avalon_mm_read_ent.vhd
+++ G3U_HW_V02_2GB/Qsys_Project/MebX_Qsys_Project/synthesis/submodules/ftdi_rx_data_avalon_mm_read_ent.vhd
@@ -21,6 +21,7 @@ end entity ftdi_rx_data_avalon_mm_read_ent;
 architecture rtl of ftdi_rx_data_avalon_mm_read_ent is
 
 	signal s_readdata_fetched : std_logic;
+	signal s_data_acquired    : std_logic;
 
 begin
 
@@ -51,13 +52,13 @@ begin
 		procedure p_readdata(read_address_i : t_ftdi_data_avalon_mm_address) is
 		begin
 			-- Registers Data Read
-			
-			buffer_rdreq_o                    <= '1';
+
+			buffer_rdreq_o <= '1';
 			case (read_address_i) is
 				-- Case for access to all registers address
 
-			when 0 to 1023 =>
-				
+				when 0 to 1023 =>
+
 					-- check if the readdata is fetched
 					if (s_readdata_fetched = '1') then
 						s_readdata_fetched                <= '0';
@@ -85,6 +86,7 @@ begin
 		if (rst_i = '1') then
 			ftdi_rx_data_avalon_mm_o.readdata    <= (others => '0');
 			ftdi_rx_data_avalon_mm_o.waitrequest <= '1';
+			s_data_acquired                      <= '0';
 			v_read_address                       := 0;
 			p_reset_registers;
 		elsif (rising_edge(clk_i)) then
@@ -92,10 +94,14 @@ begin
 			ftdi_rx_data_avalon_mm_o.waitrequest <= '1';
 			p_flags_hold;
 			p_buffer_control;
+			s_data_acquired                      <= '0';
 			if (ftdi_rx_data_avalon_mm_i.read = '1') then
-				v_read_address                       := to_integer(unsigned(ftdi_rx_data_avalon_mm_i.address));
-				ftdi_rx_data_avalon_mm_o.waitrequest <= '0';
-				p_readdata(v_read_address);
+				s_data_acquired <= '1';
+				if (s_data_acquired = '0') then
+					v_read_address                       := to_integer(unsigned(ftdi_rx_data_avalon_mm_i.address));
+					ftdi_rx_data_avalon_mm_o.waitrequest <= '0';
+					p_readdata(v_read_address);
+				end if;
 			end if;
 		end if;
 	end process p_ftdi_rx_data_avalon_mm_read;
