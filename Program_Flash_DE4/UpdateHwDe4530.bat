e32cd98c0c5aa5955793ce97c5e4c6aa56b4bd94
Rodrigo de Marca França <rodmarfran@gmail.com>
2020-02-14 14:44:05 -0300

    Added memories fast test

---
+++ Program_Flash_DE4/UpdateHwDe4530.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+PUSHD "%~dp0"
+
+REM Pastas a serem utilizadas, precisa ser ajustado conforme a necessidade
+SET HARDWARE_FOLDER=..\G3U_HW_V02_2GB\Quartus_Project\output_files
+SET HARDWARE_NAME=MebX_Quartus_Project_DE4_530.sof
+
+REM Copia o último arquivo de HW (.sof)
+ECHO   Copiando ultimo arquivo de hardware (.sof)...
+ROBOCOPY "%HARDWARE_FOLDER%" "." "%HARDWARE_NAME%" /R:0 /W:0 /NJH /NJS > NUL
+
+REM Renomeia a arquivo o timestamp
+SET HARDWARE_DATE=%date%%time%
+SET HARDWARE_DATE=%HARDWARE_DATE:/=%
+SET HARDWARE_DATE=%HARDWARE_DATE::=%
+SET HARDWARE_DATE=%HARDWARE_DATE:,=%
+SET HARDWARE_DATE=%HARDWARE_DATE%_%HARDWARE_NAME%
+REN "%HARDWARE_NAME%" "%HARDWARE_DATE%" > NUL
+
+ECHO   Copia Terminada!!
+REM PAUSE
---
+++ Program_Flash_DE4/UpdateSwSimucamR0Uart .bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+PUSHD "%~dp0"
+
+REM Pastas a serem utilizadas, precisa ser ajustado conforme a necessidade
+SET SOFTWARE_FOLDER=..\G3U_HW_V02_2GB\Qsys_Project\software\Simucam_R0_UART
+SET SOFTWARE_NAME=Simucam_R0_UART.elf
+
+REM Copia o último arquivo de SW (.elf)
+ECHO   Copiando ultimo arquivo de software (.elf)...
+ROBOCOPY "%SOFTWARE_FOLDER%" "." "%SOFTWARE_NAME%" /R:0 /W:0 /NJH /NJS > NUL
+
+REM Renomeia a arquivo o timestamp
+SET SOFTWARE_DATE=%date%%time%
+SET SOFTWARE_DATE=%SOFTWARE_DATE:/=%
+SET SOFTWARE_DATE=%SOFTWARE_DATE::=%
+SET SOFTWARE_DATE=%SOFTWARE_DATE:,=%
+SET SOFTWARE_DATE=%SOFTWARE_DATE%_%SOFTWARE_NAME%
+REN "%SOFTWARE_NAME%" "%SOFTWARE_DATE%" > NUL
+
+ECHO   Copia Terminada!!
+REM PAUSE
