c4f71383a0b65c026271ea6ed22a41bd6ca9a3f0
Tiago Sanches <tiago.eem@gmail.com>
2020-04-14 14:25:17 -0300

    Build

--- G3U_HW_V02_2GB/Qsys_Project/software/Simucam_R0_UART_bsp/system.h
+++ G3U_HW_V02_2GB/Qsys_Project/software/Simucam_R0_UART_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'MebX_Qsys_Project'
  * SOPC Builder design path: ../../MebX_Qsys_Project.sopcinfo
  *
- * Generated: Fri Mar 27 12:17:14 BRT 2020
+ * Generated: Wed Apr 08 15:06:59 BRT 2020
  */
 
 /*
@@ -2064,7 +2064,7 @@
 #define SYSID_QSYS_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_NAME "/dev/sysid_qsys"
 #define SYSID_QSYS_SPAN 8
-#define SYSID_QSYS_TIMESTAMP 1585295932
+#define SYSID_QSYS_TIMESTAMP 1585942906
 #define SYSID_QSYS_TYPE "altera_avalon_sysid_qsys"
 
 
